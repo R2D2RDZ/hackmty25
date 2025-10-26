2117102330704caad4a01399f0956e3969d155f0
R2D2RDZ <62351594+R2D2RDZ@users.noreply.github.com>
2025-10-26 04:01:30 -0600

    deployment4

--- hackmty25/vite.config.ts
+++ hackmty25/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Establece la URL base para el despliegue
-  base: '/hackmty25/', // <--- ¡Asegúrate de que esta línea esté presente!
+  base: './', // <--- ¡Asegúrate de que esta línea esté presente!
   
   // Puedes dejar el resto de la configuración vacío o con tus plugins
   plugins: [],
