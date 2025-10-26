8b2a9dfc74ff908f821f7c4d9ccab3852db187e2
Gustavo Pedraza <gustavo.pedrazadrn@uanl.edu.mx>
2025-10-26 04:28:33 -0600

    botones corregidos y boton bonificacion añadida

--- hackmty25/vite.config.ts
+++ hackmty25/vite.config.ts
@@ -1,11 +1,8 @@
-// vite.config.ts
+import { defineConfig } from 'vite'
 
-import { defineConfig } from 'vite';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Establece la URL base para el despliegue
-  base: './', // <--- ¡Asegúrate de que esta línea esté presente!
-  
-  // Puedes dejar el resto de la configuración vacío o con tus plugins
-  plugins: [],
-});
+  server: {
+    port: 5173
+  }
+})
