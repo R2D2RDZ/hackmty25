02b68f83a9bf80b17b8da8bb0bcb902e5b3e9b9a
R2D2RDZ <62351594+R2D2RDZ@users.noreply.github.com>
2025-10-26 06:46:56 -0600

    dep 19

--- hackmty25/vite.config.ts
+++ hackmty25/vite.config.ts
@@ -6,15 +6,4 @@ export default defineConfig({
   server: {
     port: 5173
   },
-  build: {
-    // Configura Rollup (el bundler interno de Vite) para múltiples entradas HTML
-    rollupOptions: {
-      input: {
-        // La entrada principal (index.html, que está en la raíz)
-        main: path.resolve(__dirname, 'index.html'), 
-        // La entrada secundaria (bonificaciones.html, que moviste a public/)
-        bonificaciones: path.resolve(__dirname, 'public/bonificaciones.html'),
-      },
-    },
-  },
 })
