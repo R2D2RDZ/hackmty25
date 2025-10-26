0f762edc9d5791f861a0bfcd860576aebdcb0480
R2D2RDZ <62351594+R2D2RDZ@users.noreply.github.com>
2025-10-26 04:00:20 -0600

    deployment 3

---
+++ hackmty25/vite.config.ts
@@ -0,0 +1,11 @@
+// vite.config.ts
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Establece la URL base para el despliegue
+  base: '/hackmty25/', // <--- ¡Asegúrate de que esta línea esté presente!
+  
+  // Puedes dejar el resto de la configuración vacío o con tus plugins
+  plugins: [],
+});
